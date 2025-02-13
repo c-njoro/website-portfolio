b35fedb4dd75d2ba91fa6566e2e13d00e638ad2e
Njoroge Charles <njorodev1319@gmail.com>
2025-02-13 12:10:32 +0300

    trying sitemap pages gain

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://dev-charles.vercel.app",
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://dev-charles.vercel.app/sitemap.xml"],
-  },
-  exclude: ["/server-sitemap.xml"], // Add any pages you want to exclude
-};
